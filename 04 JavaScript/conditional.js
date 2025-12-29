44fa4839b6917a2dffa057fecbc3e709af69bf6d
sekharuppuluri <sekharuppuluri3@gmail.com>
2025-12-29 14:04:15 +0530

    Add conditional statements examples to conditional.js

---
+++ 04 JavaScript/conditional.js
@@ -0,0 +1,38 @@
+// Conditional Statements
+// conditional statements are used to perform different actions based on different conditions.
+/*
+
+    1. if statement: It executes a block of code if a specified condition is true.
+    2. if...else statement: It executes one block of code if a specified condition is true, and another block if it is false.
+    3. else if statement: It specifies a new condition to test if the first condition is false.
+    4. switch statement: It is used to perform different actions based on different conditions. It is often used as an alternative to multiple if...else if statements when you have many conditions to check against a single variable.
+    5. Ternary operator: It is a shorthand for the if...else statement and is used to assign a value based on a condition in a single line.
+
+*/
+
+// 1. if statement
+let age = 20;
+if (age >= 18) {
+    console.log("You are an adult");
+}
+
+// 2. if...else statement
+if (age >= 18) {
+    console.log("You are an adult");
+} else {
+    console.log("You are a minor");
+}
+
+// 3. else if statement
+if (age >= 18) {
+    console.log("You are an adult");
+} else if (age >= 13) {
+    console.log("You are a teenager");
+} else {
+    console.log("You are a child");
+}
+
+
+
+
+
