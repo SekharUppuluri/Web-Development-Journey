e40bb9f4a4b6a974b451fc3dd2339ec9403cad7b
sekharuppuluri <sekharuppuluri3@gmail.com>
2025-12-29 14:28:51 +0530

    Enhance examples in 02-Datatypes.js by adding console logs for each data type

--- 04 JavaScript/02-Datatypes.js
+++ 04 JavaScript/02-Datatypes.js
@@ -15,25 +15,33 @@
 
 // a) Number: Represents both integer and floating-point numbers.
 let num = 42; 
+console.log(typeof num);   
+
+// we can use the `typeof` operator to check the data type of a variable.
 
 // b) String: Represents a sequence of characters.
 let greeting = "Hello, world!"; 
+console.log(typeof greeting);
 
 // c) Boolean: Represents a logical entity and can have two values: true or false.
 let isJavaScriptFun = true;  
+console.log(typeof isJavaScriptFun);
 
 // d) Undefined: A variable that has been declared but not assigned a value.
 let undefinedVar;
+console.log(typeof undefinedVar);
 
 // e) Null: Represents the intentional absence of any object value.
-let nullVar = null; 
+let nullVar = null;
+console.log(typeof nullVar); // Note: typeof null returns "object" due to a historical bug in JavaScript.
 
 // f) Symbol: A unique and immutable primitive value used as the key of an object property.
 let sym = Symbol("unique"); 
+console.log(typeof sym);
 
 // g) BigInt: Represents integers with arbitrary precision.
 let bigIntNum = 9007199254741991n;
-
+console.log(typeof bigIntNum);
 
 // 2. Non-Primitive (Reference) Data Types:
 // These data types are more complex and can store collections of values or more complex entities. They include:
@@ -48,31 +56,37 @@ let bigIntNum = 9007199254741991n;
 */
 // a) Object:
 let person = { name: "Alice", age: 30 };
+console.log(typeof person);
 
 // b) Array:
 let numbers = [1, 2, 3, 4, 5];
+console.log(typeof numbers);
 
 // c) Function:
 function add(a, b) {
   return a + b;
 }
+console.log(typeof add);
 
 // d) Date:
 let currentDate = new Date();
+console.log(typeof currentDate);
 
 // e) RegExp:
 let pattern = /ab+c/;
+console.log(typeof pattern);
 
 // f) Map:
 let map = new Map();
 map.set("key1", "value1");
 map.set("key2", "value2");
+console.log(typeof map);
 
 // g) Set:
 let set = new Set();
 set.add(1);
 set.add(2);
 set.add(2); // Duplicate, will not be added
+console.log(typeof set);
 
 // Note: JavaScript is a dynamically typed language, meaning that variables can hold values of any data type and can change types at runtime.
-
