ae515f36f06729667131da1e1f4d8dcd9622874c
sekharuppuluri <sekharuppuluri3@gmail.com>
2025-12-29 16:49:42 +0530

    Add Array.js file with examples of array creation, access, and modification in JavaScript

---
+++ 04 JavaScript/Array.js
@@ -0,0 +1,17 @@
+// Arrays in JavaScript
+// An array is a special type of object used to store ordered collections of values. Each value in an array has a numeric index, starting from 0.
+
+// Creating an array
+let fruits = ["Apple", "Banana", "Cherry", "Date"];
+console.log("Fruits Array:", fruits);
+
+// Accessing array elements
+let firstFruit = fruits[0];
+console.log("First Fruit:", firstFruit);
+
+// Modifying array elements
+console.log(fruits[1]);
+fruits[1] = "Blueberry";
+console.log("Modified Fruits Array:", fruits);
+
+
