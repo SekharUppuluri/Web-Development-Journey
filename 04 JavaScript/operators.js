a5583f05884c30665165074306d3d7807bbb24e2
sekharuppuluri <sekharuppuluri3@gmail.com>
2025-12-30 07:54:25 +0530

    Add operators.js file to document various types of operators in JavaScript

---
+++ 04 JavaScript/operators.js
@@ -0,0 +1,14 @@
+// ====== Operators in JavaScript ======
+// Operators are special symbols or keywords that perform operations on operands (values or variables). JavaScript supports various types of operators, including:
+/*
+    1. Arithmetic Operators: Used for mathematical calculations.
+    2. Assignment Operators: Used to assign values to variables.
+    3. Comparison Operators: Used to compare two values.
+    4. Logical Operators: Used to combine multiple conditions.
+    5. Bitwise Operators: Used to perform operations on binary numbers.
+    6. Conditional (Ternary) Operator: A shorthand for if...else statements.
+    7. Nullish Coalescing Operator: Used to provide a default value when dealing with null or undefined.
+
+*/
+
+
